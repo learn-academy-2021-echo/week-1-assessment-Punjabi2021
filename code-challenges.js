97876b32a7c6c3738b0ad22b9afa3492ebf3be12
Punjabi2021 <93889575+Punjabi2021@users.noreply.github.com>
2021-12-06 00:11:41 -0800

    finished the assessment for week 1

--- code-challenges.js
+++ code-challenges.js
@@ -6,34 +6,68 @@
 // If you get stuck, please leave comments to help us understand your thought process.
 
 // --------------------1) Create a statement that determines if a temperature is below boiling point, at boiling point, or above boiling point. Boiling point is 212 degrees Fahrenheit.
-
+//If the var is above 212 it will return a value of Above boiling point
+//If the var is at 212 it will return a value of boiling point
+// If the var is  below boiling point it will return a value of below boiling
 // Create the code and test each of the variables provided. Expected output: "42 is below boiling point", "350 is above boiling point", "212 is at boiling point"
 
-var temp = 42
-// var temp = 350
-// var temp = 212
+//var temp1 = 42
+//var temp2 = 350
+//var temp3 = 212
 
+//if(temp1 === 212) {
+  //console.log("Boiling point.")
+//} else if(temp1 > 212) {
+  //console.log("Above boiling point")
+//} else {
+  //console.log("Below boiling point")
+//}
 
 // --------------------2) Create the code that will combine the two arrays and return the length using the variables provided below. Expected output: 10
 
-var myNumbers1 = [3, 7, 0, 36, -9]
-var myNumbers2 = [8, -7, 42, 9, 13]
+//var myNumbers1 = [3, 7, 0, 36, -9]
+//var myNumbers2 = [8, -7, 42, 9, 13]
+
+
+
+
+//let array1 = [3, 7, 0, 36, -9];
+//let array2 = [8, -7, 42, 9, 13];
+//let array3 = array1.concat(array2);
+
+//console.log(array3.length);
 
 
 // --------------------3) Create the code that will reverse the letters of a string using the test variable provided below. Expected output: "1202 ohcE"
 
-var currentCohort = "Echo 2021"
-
+//var currentCohort = "Echo 2021"
+//console.log("Echo 2021".split("").reverse().join(""));
 
 // --------------------4) Create a statement that will evaluate whether each number in an array is even or odd. Use the test variable provided below. Expected output: "odd" "even" "odd" "even" "odd" "even"
+//var myArray = [13, 34, 5, 10, 27, 42];
 
-var myArray = [13, 34, 5, 10, 27, 42]
-
+//for(let index = 0; index < myArray.length; index++)
+//{
+  //if(myArray[index] % 2 === 0) {
+    //console.log("even")
+  //} else if(myArray[index] % 2 === 1) {
+    //console.log("odd")
+  //}
+//}
 
 // --------------------5) Create a statement that evaluates two numbers and subtracts the smaller number from the larger number using the two sets of test variables provided below. Expected output: 42, 3
 
-var number1 = 58
-var number2 = 100
+//let y = 100;
+//let x = 58;
 
-// var number1 = 27
-// var number2 = 24
+//let z = y - x;
+
+//console.log(z);
+
+
+let y = 27
+let x = 24
+
+let z = y - x;
+
+console.log(z);
--- coding-concepts.js
+++ coding-concepts.js
@@ -7,45 +7,45 @@
 
 // --------------------1) What will this log?
 
-var cohort = "Echo 2021"
-// console.log(cohort.length)
+//var cohort = "Echo 2021"
+ //console.log(cohort.length)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer:Each letter will be on a different line.
+// b) Verify and explain:The answer was 9 because the comand was length it determined thespaces the length was inside the parentheses.
 
 
 // --------------------2) What will this log?
 
-var greeting = "Hello World!"
-// console.log(greeting[3])
+//var greeting = "Hello World!"
+ //console.log(greeting[3])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer:"l"
+// b) Verify and explain:The number three inside of the square brakets will decide when we stop counting we end on the l .
 
 
 // --------------------3) What will this log?
 
-var languages = ["JavaScript", "Ruby", "Python", "C++"]
-var index = 1
+//var languages = ["JavaScript", "Ruby", "Python", "C++"]
+//var index = 1
 // console.log(languages[index])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer:"JavaScript"
+// b) Verify and explain:"Ruby" computers start counting with a zero I forgot the simple principle resulted in the wrong answer.
 
 
 // --------------------4) What will this log?
 
-var weekendDays = ["saturday", "sunday"]
+//var weekendDays = ["saturday", "sunday"]
 // console.log(weekendDays.toUpperCase())
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer:"SATURDAY","SUNDAY"
+// b) Verify and explain:"weekendDays.ToUpperCase is not a function" the computer needs an understandabe code for it to work and these commands are Invalid
 
 
 // --------------------5) What will this log?
 
-var dataTypes = ["number", "string", "Boolean"]
+//var dataTypes = ["number", "string", "Boolean"]
 // console.log(typeof dataTypes.length)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer:number
+// b) Verify and explain:String is in parentheses, Boolean is true or false so i feel like the only logical answer is number.
